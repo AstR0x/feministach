487e7823e60181888a7d3623bc05a5cf54029416
Vlad <astr0xy1@gmail.com>
2020-05-12 02:48:30 +0300

    добавил страницу для неверных роутов

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h1 class="not-found">404 Not Found</h1>
+    <img class="icon" src="../icons/Not-Found.svg" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound',
+  };
+</script>
+
+<style scoped>
+  .container {
+    text-align: center;
+    margin-top: 10%;
+  }
+
+  .icon {
+    margin-top: 50px;
+    width: 25%;
+  }
+
+  @media (max-width: 414px) {
+    .container {
+      text-align: center;
+      margin-top: 30%;
+    }
+
+    .not-found {
+      font-size: 24px;
+    }
+
+    .icon {
+      margin-top: 20px;
+      width: 45%;
+    }
+  }
+
+</style>
