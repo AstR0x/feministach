4a4a652e538a8ed1c9b091cb58744d3a407ab527
Vlad <astr0xy1@gmail.com>
2020-05-30 13:20:51 +0300

    починил баг с повторяющимся айди у поста

--- utils/matchRepliesIds.js
+++ utils/matchRepliesIds.js
@@ -1,7 +1,7 @@
 const matchRepliesIds = (fromRepliesIds, post, commentId) => {
   fromRepliesIds.forEach(fromReplayId => {
     if (fromReplayId === post.id) {
-      post.toRepliesIds.push(fromReplayId);
+      post.toRepliesIds.push(commentId);
     } else {
       const foundComment = post.comments.find(comment => comment.id === fromReplayId);
       foundComment.toRepliesIds.push(commentId);
